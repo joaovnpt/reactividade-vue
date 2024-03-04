3eb1886e47666d1e369710e552e332c4b366adb3
joaovnpt <joaovitorguiotti8@gmail.com>
2024-03-04 15:22:04 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<script setup>
+ import { ref } from 'vue';
+
+const contador = ref(0);
+
+function add() {
+  contador.value++;
+}
+
+function minus() {
+  contador.value--;
+}
+
+function reset() {
+  contador.value = 0;
+}
+
+</script>
+
+<template>
+<div class="contador1">
+  <button @click="add()">+</button>
+  <p>{{ contador }}</p>
+  <button @click="minus()">-</button>
+  <button @click="reset()">C</button>
+</div>
+<div class="contador2">
+  <button @click="add()">+</button>
+  <p>{{ contador }}</p>
+  <button @click="add()">-</button>
+  <button @click="add()">C</button>
+</div>
+</template>
+
+<style scoped>
+.contador1 {
+  background-color: black;
+  padding: 25px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+p {
+  font-size: larger;
+  color: white;
+  margin-right: 10px;
+  margin-left: 10px;
+}
+
+</style>
